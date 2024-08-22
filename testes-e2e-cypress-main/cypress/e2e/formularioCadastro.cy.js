2f9dd5f9633b62e6aa5bda030c31b6cab03656ba
Lorifersi <lorenafersilva17@gmail>
2024-08-22 13:40:30 -0300

    Feito os testes para cadastro

--- testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
@@ -5,17 +5,54 @@ describe('Formulário cadastro', () => {
     it('Usuário deve conseguir se cadastrar com sucesso', () => {
     cy.getByData('botao-cadastro').click() /* testaremos agora o botão 'cadastrar' */
     cy.getByData('nome-input').type('Jose da Silva')
-    cy.getByData('email-input').type('f@email.com')
+    cy.getByData('email-input').type('3@email.com')
     cy.getByData('senha-input').type('456789')
     cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-sucesso').should('exist')
-    //.and('have-text', 'Usuário cadastrado com sucesso!')
-    
-    /* a mensagem exibida deve ser: "Usuário cadastrado com sucesso!" */
-    
-    /* clicamos no botão cadastro e inserimos os dados de cadastro para testar o cenário feliz, onde o cadastro
-    
-    será realizado com sucesso. */
-    
+    cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!')
+
     })
+
+    it('Não deve permiter o campo nome em branco', ()=> {
+        cy.visit('/')
+        cy.getByData('botao-cadastro').click()
+        //cy.getByData('nome-input').type('Jose da Silva')
+        cy.getByData('email-input').type('neilton@alura.com') /* não passaremos nenhuma informação no campo email, comentando esta linha, pois o comando type não aceita parâmetro vazio */
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de nome é obrigatório') /* a
+    
+    // mensagem exibida deve ser: "o campo nome é obrigatório" */
+    })
+
+    it('Não deve permitir um e-mail inválido!', () => {
+        cy.getByData('botao-cadastro').click()
+        cy.getByData('email-input').type('neilton@alura')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+    })
+
+    it('Não deve permiter o campo email em branco', ()=> {
+        cy.visit('/')
+        cy.getByData('botao-cadastro').click()
+        //cy.getByData('nome-input').type('Jose da Silva')
+        //cy.getByData('email-input').type('') /* não passaremos nenhuma informação no campo email, comentando esta linha, pois o comando type não aceita parâmetro vazio */
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório') /* a
+    
+    // mensagem exibida deve ser: "o campo email é obrigatório" */
+    })
+
+    it('Não deve permiter o campo senha em branco', ()=> {
+        cy.visit('/')
+        cy.getByData('botao-cadastro').click()
+        //cy.getByData('nome-input').type('Jose da Silva')
+        cy.getByData('email-input').type('neilton@alura.com') /* não passaremos nenhuma informação no campo email, comentando esta linha, pois o comando type não aceita parâmetro vazio */
+        //cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de senha é obrigatório') /* a
+    
+    // mensagem exibida deve ser: "o campo email é obrigatório" */
+    })
+
     })

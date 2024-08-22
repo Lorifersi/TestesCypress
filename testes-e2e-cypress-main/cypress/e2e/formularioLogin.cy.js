a0f824c9917b87587fd809ac0027411209fff2dc
Lorifersi <lorenafersilva17@gmail>
2024-08-22 09:55:01 -0300

    Teste de email inválido

--- testes-e2e-cypress-main/cypress/e2e/formularioLogin.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formularioLogin.cy.js
@@ -1,15 +1,41 @@
-describe('Validaçãop de login', ()=> {
-    it.only('Não deve permiter um campo em branco', ()=> {
+describe('Formulário de login', () => { // contexto do nosso teste
+    beforeEach(()=> {
+    cy.visit('http://localhost:3000')
+    // antes de qualquer teste, é necessário visitar o site da aplicação. Por isto, utilizamos o hook beforeEach
+    // PS: NUNCA altere as portas
+    })
+    it('Não deve permitir um e-mail inválido!', () => {
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    
+    /* clicamos no botão login e inserimos os dados de login para testar o cenário triste, onde o email está inválido */
+    })
+
+    it('Não deve permiter o campo email em branco', ()=> {
         cy.visit('/')
         cy.getByData('botao-login').click()
         // cy.getByData('email-input').type('') /* não passaremos nenhuma informação no campo email, comentando esta linha, pois o comando type não aceita parâmetro vazio */
         cy.getByData('senha-input').type('123456')
         cy.getByData('botao-enviar').click()
-        cy.getByData('mensagem-erro').should('exist')
-        // .and('have-text', 'o campo email é obrigatório') /* a
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório') /* a
+    
+    // mensagem exibida deve ser: "o campo email é obrigatório" */
+    })
+
+    
+    it('Não deve permiter o campo senha em branco', ()=> {
+        cy.visit('/')
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com') /* não passaremos nenhuma informação no campo email, comentando esta linha, pois o comando type não aceita parâmetro vazio */
+        //cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de senha é obrigatório') /* a
     
     // mensagem exibida deve ser: "o campo email é obrigatório" */
     })
 })
+
 
 
